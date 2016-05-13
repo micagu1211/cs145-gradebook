de0def2a1087ce9988c6f2f05c74c1d0773d8ad4
Michael Aguilar <MikeAguilar@Michaels-MacBook.local>
2016-05-13 10:41:00 -0600

    Completed HW

--- GradebookApp.java
+++ GradebookApp.java
@@ -1,9 +1,25 @@
 // GradebookApp.java v.20160502
-
 class GradebookApp {
-	
-	public static void main(String[] args) {
-
+	static double[] grades(){
+		System.out.println("HERE ARE YOUR GRADES");
+		double[] randGrades= new double[10];
+		for(int i=0; i<randGrades.length; i++){
+			randGrades[i]=(Math.random()*100)+1;
+			System.out.print(randGrades[i]+"\n");
+		}
+		return randGrades;
 	}
+
+	public static void main(String[] args) {
+		double[] m=grades();
+		String n=Student.firstName();
+		System.out.println("HELLO " + n);
+		double x=Student.high(m);
+		double y=Student.low(m);
+		double z=Student.average(m);
+		char g=Student.letterGrade(m);
+		System.out.print("YOUR HIGH: " + x + "\n\n" + "YOUR LOW: " + y + "\n\n" + "YOUR AVERAGE: " + z+"\n" + "YOUR LETTER GRADE: " + g + "\n");
+	}
+	//436 //350
 
 }
--- Student.java
+++ Student.java
@@ -1,5 +1,80 @@
-// Student.java v.20160502
+import java.util.Scanner;
+import java.util.Random;
 
 class Student {
+
+	static String firstName(){
+		Scanner scnr = new Scanner(System.in);
+        System.out.print("Enter your first name please: ");
+        String name = scnr.next();
+		return name;
+	}
+
+	/*static double[] grades(){
+		double[] randGrades= new double[10];
+		for(int i=0; i<randGrades.length; i++){
+			randGrades[i]=(Math.random()*100)+1;
+			System.out.print(randGrades[i]+"\n");
+		}
+		return randGrades;
+	}*/
+
+
+	static double high(double[] grades) {
+        double high=grades[0];
+        for(int i=0; i<grades.length; i++){
+            if(grades[i]>high){
+                high= grades[i];
+            }
+        }
+
+      	return high;
+
+       }
+		//System.out.println("YOUR HIGHEST GRADE: " + high + "\n");
+
+	static double low(double[] grades){ 
+		double low=grades[0];
+        for(int i=0; i<grades.length; i++){
+            if(grades[i]<low){
+                low=grades[i];
+            }
+        }
+        return low;
+       }
+		//System.out.println("YOUR LOWEST GRADE: " + low + "\n");
+
+	static double average(double[] grades){
+		double sum=0;
+
+        for(int i=0; i<grades.length; i++)
+            sum=sum+grades[i];
+
+        double average=sum/grades.length;
+
+		return average;
+
+	}
+	
+	static char letterGrade(double[] grades){
+		char x='-';
+		double y= average(grades);
+		if(y>90 && y<100){
+			x='A';
+		}
+		else if(y>80 && y<89.99){
+			x='B';
+		}
+		else if(y>70 && y<79.99){
+			x='C';
+		}
+		else if(y>60 && y<69.99){
+			x='D';
+		}
+		else if(y<60){
+			x='F';
+		}
+		return x;
+	}
 
 }
